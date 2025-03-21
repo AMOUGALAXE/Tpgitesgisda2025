030c5434c2f7e16c989610b37b9d06294efbd843
saida <saidakoli501@gmail.com>
2025-03-21 09:38:00 +0000

    MAJ2 du fichier text

--- text.java
+++ text.java
@@ -2,4 +2,34 @@ public class text {
     private string nom;
     private string prenom;
     private  int age;
+
+    public text(string nom, string prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+
+    public string getNom() {
+        return nom;
+    }
+
+    public void setNom(string nom) {
+        this.nom = nom;
+    }
+
+    public string getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(string prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
